9fb0767313e4720e62aff1eaf00fa2b6a8fd62aa
Hariom Singh <hariomsingh0398@gmail.com>
2024-06-27 11:27:51 +0530

    Hello world in java

---
+++ hello.java
@@ -0,0 +1,8 @@
+
+//basic hello world program in java
+public class hello {
+    public static void main(String[] arr) {
+        System.out.println("Hello, world!");
+        System.out.println("Java first program");
+    }
+}
